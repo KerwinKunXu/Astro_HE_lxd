cea437da192527923a00801afd29c77eef845607
KerwinKunXu <76147563+KerwinKunXu@users.noreply.github.com>
2021-03-05 19:54:33 +0800

    Add files via upload

---
+++ ideas/ULX/ULXP/NGC5907/code/ngc5907.f90
@@ -0,0 +1,29 @@
+program ngc5907
+
+implicit none
+
+!constant
+real(8),parameter :: g=6.67d-8
+real(8),parameter :: msun=2d33
+real(8),parameter :: mdotedd=1.946d18
+
+real(8) :: mns=2.8d33
+real(8),parameter :: rns=1d6
+real(8) :: 
+ 
+!parameters
+real(8) :: mdot
+
+mdot0=
+t0=
+alpha=
+
+mdot=mdot0/(t/t0)**alpha
+!-19/14<alpha<-8/7, i.e. -1.35714~-1.14286
+
+rin=xi*mu**0.571429/(2*g*mns)**0.142857/mdot**0.28571
+rco=(g*m)**0.333333/(4*pi**2)**0.333333*p**0.666667
+
+torque=
+
+end
